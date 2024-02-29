f3188e0120f20c2198ab3dd551bc773b45d1e337
hansanghee <hansanghee365@gmail.com>
2024-02-29 13:47:11 +0900

    third commit

--- src/components/BlogPage.js
+++ src/components/BlogPage.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 const BlogPage = () => {
   return (
-    <div>BlogPage</div>
+    <div className="container">
+      <h1>blog</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet sint saepe ratione eum iste ab incidunt cupiditate ipsum fugit reprehenderit! Quas itaque fugiat adipisci corporis voluptatibus nemo vero, numquam fuga.</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet sint saepe ratione eum iste ab incidunt cupiditate ipsum fugit reprehenderit! Quas itaque fugiat adipisci corporis voluptatibus nemo vero, numquam fuga.</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet sint saepe ratione eum iste ab incidunt cupiditate ipsum fugit reprehenderit! Quas itaque fugiat adipisci corporis voluptatibus nemo vero, numquam fuga.</p>
+    </div>
   )
 }
 
--- src/components/Sns.js
+++ src/components/Sns.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Sns = () => {
   return (
-    <div>Sns</div>
+    <div className="container">
+      <h1>SNS</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis ab enim magni, rem tempore similique quae! Accusamus adipisci quibusdam doloremque perspiciatis incidunt sed assumenda, asperiores rerum, delectus et aut optio saepe? Modi sint totam nulla error perferendis, aperiam officiis temporibus eligendi sunt non voluptatibus eveniet quidem asperiores excepturi in recusandae! Perferendis pariatur in quasi, itaque accusantium quos? Adipisci incidunt tempora, perferendis sunt sit ratione ea sequi pariatur unde labore odit, facilis voluptatum est. Dolore consequatur quisquam sequi vel distinctio modi, exercitationem possimus, animi saepe ipsa est earum! Doloribus velit quo earum obcaecati, itaque quos recusandae illo possimus magnam repellat dolores.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis ab enim magni, rem tempore similique quae! Accusamus adipisci quibusdam doloremque perspiciatis incidunt sed assumenda, asperiores rerum, delectus et aut optio saepe? Modi sint totam nulla error perferendis, aperiam officiis temporibus eligendi sunt non voluptatibus eveniet quidem asperiores excepturi in recusandae! Perferendis pariatur in quasi, itaque accusantium quos? Adipisci incidunt tempora, perferendis sunt sit ratione ea sequi pariatur unde labore odit, facilis voluptatum est. Dolore consequatur quisquam sequi vel distinctio modi, exercitationem possimus, animi saepe ipsa est earum! Doloribus velit quo earum obcaecati, itaque quos recusandae illo possimus magnam repellat dolores.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis ab enim magni, rem tempore similique quae! Accusamus adipisci quibusdam doloremque perspiciatis incidunt sed assumenda, asperiores rerum, delectus et aut optio saepe? Modi sint totam nulla error perferendis, aperiam officiis temporibus eligendi sunt non voluptatibus eveniet quidem asperiores excepturi in recusandae! Perferendis pariatur in quasi, itaque accusantium quos? Adipisci incidunt tempora, perferendis sunt sit ratione ea sequi pariatur unde labore odit, facilis voluptatum est. Dolore consequatur quisquam sequi vel distinctio modi, exercitationem possimus, animi saepe ipsa est earum! Doloribus velit quo earum obcaecati, itaque quos recusandae illo possimus magnam repellat dolores.</p>
+    </div>
   )
 }
 
